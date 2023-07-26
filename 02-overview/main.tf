9d5623c340dbe98daf1902bcb42be501080a8796
prajwal <prajwal.j91@gmail.com>
2023-07-26 11:27:50 +0000

    modified:   02-overview/main.tf

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  ami           = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 LTS // us-east-1
   instance_type = "t2.micro"
 }
